d4603e19f04f347b22680d089acabe37bed27c51
Павел Шлыков <shlicha@yandex.ru>
2024-12-11 15:21:06 +0300

    Module_19. Admin

--- GameShop/task1/admin.py
+++ GameShop/task1/admin.py
@@ -5,9 +5,16 @@ from .models import Buyer, Game
 
 @admin.register(Buyer)
 class BuyerAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
 
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 20
